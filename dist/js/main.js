b3d6e0c618d3d160230b4038b2e5c6f8c471b728
denieall j <denieall.joe@gmail.com>
2018-09-13 23:54:12 +0800

    Add menu button then style and animate it

---
+++ dist/js/main.js
@@ -0,0 +1,36 @@
+// Get items from DOM
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set Initial State Of Menu
+let showMenu = false;
+
+// Toggle menu button
+menuBtn.addEventListener("click", () => {
+	if (!showMenu) {
+		menu.classList.add('show');
+		menuBtn.classList.add('close');
+		menuNav.classList.add('show');
+		menuBranding.classList.add('show');
+
+		navItems.forEach((item) => {
+			item.classList.add('show');
+		});
+
+		showMenu = true;
+	} else {
+		menu.classList.remove('show');
+		menuBtn.classList.remove('close');
+		menuNav.classList.remove('show');
+		menuBranding.classList.remove('show');
+
+		navItems.forEach(item => {
+			item.classList.remove('show');
+		});
+
+		showMenu = false;
+	}
+});
